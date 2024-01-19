7d9fc326680a613290cf6d4dcd35a921fd4473d3
NikeRummer <115785602+NikeRummer@users.noreply.github.com>
2024-01-19 16:45:40 +0300

    1

--- Кп_Версия 2/Кп_Версия 2/FileName.c
+++ Кп_Версия 2/Кп_Версия 2/FileName.c
@@ -28,83 +28,83 @@ int main() {
     system("chcp 1251");
 
     do {
-        printf("Меню:\n");
-        printf("1) Добавить запись\n");
-        printf("2) Поиск по названию\n");
-        printf("3) Поиск по стоимости блока\n");
-        printf("4) Поиск по площади строительного материала\n");
-        printf("5) Сортировка по теплопроводимости\n");
-        printf("6) Записать в файл\n");
-        printf("7) Выход из программы\n");
-        printf("Выберите пункт меню: ");
+        printf("РњРµРЅСЋ:\n");
+        printf("1) Р”РѕР±Р°РІРёС‚СЊ Р·Р°РїРёСЃСЊ\n");
+        printf("2) РџРѕРёСЃРє РїРѕ РЅР°Р·РІР°РЅРёСЋ\n");
+        printf("3) РџРѕРёСЃРє РїРѕ СЃС‚РѕРёРјРѕСЃС‚Рё Р±Р»РѕРєР°\n");
+        printf("4) РџРѕРёСЃРє РїРѕ РїР»РѕС‰Р°РґРё СЃС‚СЂРѕРёС‚РµР»СЊРЅРѕРіРѕ РјР°С‚РµСЂРёР°Р»Р°\n");
+        printf("5) РЎРѕСЂС‚РёСЂРѕРІРєР° РїРѕ С‚РµРїР»РѕРїСЂРѕРІРѕРґРёРјРѕСЃС‚Рё\n");
+        printf("6) Р—Р°РїРёСЃР°С‚СЊ РІ С„Р°Р№Р»\n");
+        printf("7) Р’С‹С…РѕРґ РёР· РїСЂРѕРіСЂР°РјРјС‹\n");
+        printf("Р’С‹Р±РµСЂРёС‚Рµ РїСѓРЅРєС‚ РјРµРЅСЋ: ");
         scanf("%d", &choice);
 
         switch (choice) {
         case 1:
             if (size < MAX_SIZE) {
-                printf("Введите данные для новой записи в программе:\n");
-                printf("Название: ");
+                printf("Р’РІРµРґРёС‚Рµ РґР°РЅРЅС‹Рµ РґР»СЏ РЅРѕРІРѕР№ Р·Р°РїРёСЃРё РІ РїСЂРѕРіСЂР°РјРјРµ:\n");
+                printf("РќР°Р·РІР°РЅРёРµ: ");
                 scanf("%s", materials[size].name);
-                printf("Цена материала: ");
+                printf("Р¦РµРЅР° РјР°С‚РµСЂРёР°Р»Р°: ");
                 scanf("%d", &materials[size].price);
-                printf("Площадь одного блока: ");
+                printf("РџР»РѕС‰Р°РґСЊ РѕРґРЅРѕРіРѕ Р±Р»РѕРєР°: ");
                 scanf("%lf", &materials[size].square);
-                printf("Плотность кг/м3: ");
+                printf("РџР»РѕС‚РЅРѕСЃС‚СЊ РєРі/Рј3: ");
                 scanf("%lf", &materials[size].density);
-                printf("Прочность от 1 до 200 кг/см: ");
+                printf("РџСЂРѕС‡РЅРѕСЃС‚СЊ РѕС‚ 1 РґРѕ 200 РєРі/СЃРј: ");
                 scanf("%d", &materials[size].endurance);
-                printf("Теплопроводность от 0 до 0,9 ВТ/м: ");
+                printf("РўРµРїР»РѕРїСЂРѕРІРѕРґРЅРѕСЃС‚СЊ РѕС‚ 0 РґРѕ 0,9 Р’Рў/Рј: ");
                 scanf("%lf", &materials[size].thermal_conductivity);
                 size++;
             }
             else {
-                printf("База данных переполненна! Добавление запрещено! Ошибка!\n");
+                printf("Р‘Р°Р·Р° РґР°РЅРЅС‹С… РїРµСЂРµРїРѕР»РЅРµРЅРЅР°! Р”РѕР±Р°РІР»РµРЅРёРµ Р·Р°РїСЂРµС‰РµРЅРѕ! РћС€РёР±РєР°!\n");
             }
             break;
         case 2:
             if (size > 0) {
                 char Find_of_Name[40];
-                printf("Введите наименование строительного материала для поиска: ");
+                printf("Р’РІРµРґРёС‚Рµ РЅР°РёРјРµРЅРѕРІР°РЅРёРµ СЃС‚СЂРѕРёС‚РµР»СЊРЅРѕРіРѕ РјР°С‚РµСЂРёР°Р»Р° РґР»СЏ РїРѕРёСЃРєР°: ");
                 scanf("%s", &Find_of_Name);
                 Find_Name(materials, size, Find_of_Name);
             }
             else {
-                printf("База данных = пустое место!\n");
+                printf("Р‘Р°Р·Р° РґР°РЅРЅС‹С… = РїСѓСЃС‚РѕРµ РјРµСЃС‚Рѕ!\n");
             }
             break;
         case 3:
             if (size > 0) {
                 int Find_of_Price1, Find_of_Price2;
-                printf("Введите начальную цену строительного материала для поиска: ");
+                printf("Р’РІРµРґРёС‚Рµ РЅР°С‡Р°Р»СЊРЅСѓСЋ С†РµРЅСѓ СЃС‚СЂРѕРёС‚РµР»СЊРЅРѕРіРѕ РјР°С‚РµСЂРёР°Р»Р° РґР»СЏ РїРѕРёСЃРєР°: ");
                 scanf("%d", &Find_of_Price1);
-                printf("Введите конечную цену строительного материала для поиска: ");
+                printf("Р’РІРµРґРёС‚Рµ РєРѕРЅРµС‡РЅСѓСЋ С†РµРЅСѓ СЃС‚СЂРѕРёС‚РµР»СЊРЅРѕРіРѕ РјР°С‚РµСЂРёР°Р»Р° РґР»СЏ РїРѕРёСЃРєР°: ");
                 scanf("%d", &Find_of_Price2);
                 Find_Price(materials, size, Find_of_Price1, Find_of_Price2);
             }
             else {
-                printf("База данных = пустое место!\n");
+                printf("Р‘Р°Р·Р° РґР°РЅРЅС‹С… = РїСѓСЃС‚РѕРµ РјРµСЃС‚Рѕ!\n");
             }
             break;
         case 4:
             if (size > 0) {
                 double Find_of_Square;
-                printf("Введите площадь строительного материала для поиска: ");
+                printf("Р’РІРµРґРёС‚Рµ РїР»РѕС‰Р°РґСЊ СЃС‚СЂРѕРёС‚РµР»СЊРЅРѕРіРѕ РјР°С‚РµСЂРёР°Р»Р° РґР»СЏ РїРѕРёСЃРєР°: ");
                 scanf("%lf", &Find_of_Square);
                 Find_Square(materials, size, Find_of_Square);
             }
             else {
-                printf("База данных = пустое место!\n");
+                printf("Р‘Р°Р·Р° РґР°РЅРЅС‹С… = РїСѓСЃС‚РѕРµ РјРµСЃС‚Рѕ!\n");
             }
             break;
         case 5:
             if (size > 0) {
                 double Sort_of_Thermal_conductivity;
-                printf("Введите предел теплопроводимости: ");
+                printf("Р’РІРµРґРёС‚Рµ РїСЂРµРґРµР» С‚РµРїР»РѕРїСЂРѕРІРѕРґРёРјРѕСЃС‚Рё: ");
                 scanf("%lf", &Sort_of_Thermal_conductivity);
                 Sort_Thermal_conductivity(materials, size, Sort_of_Thermal_conductivity);
             }
             else {
-                printf("База данных = пустое место!\n");
+                printf("Р‘Р°Р·Р° РґР°РЅРЅС‹С… = РїСѓСЃС‚РѕРµ РјРµСЃС‚Рѕ!\n");
             }
             break;
         case 6:
@@ -112,77 +112,77 @@ int main() {
                 Write_to_File(materials, size);
             }
             else {
-                printf("База данных = пустое место!\n");
+                printf("Р‘Р°Р·Р° РґР°РЅРЅС‹С… = РїСѓСЃС‚РѕРµ РјРµСЃС‚Рѕ!\n");
             }
             break;
         case 7:
-            printf("Программа прекратила работу и была успешно завершена!\n");
+            printf("РџСЂРѕРіСЂР°РјРјР° РїСЂРµРєСЂР°С‚РёР»Р° СЂР°Р±РѕС‚Сѓ Рё Р±С‹Р»Р° СѓСЃРїРµС€РЅРѕ Р·Р°РІРµСЂС€РµРЅР°!\n");
             break;
         default:
-            printf("Неправильный ввод. Пожалуйста, выберите существующий пункт меню.\n");
+            printf("РќРµРїСЂР°РІРёР»СЊРЅС‹Р№ РІРІРѕРґ. РџРѕР¶Р°Р»СѓР№СЃС‚Р°, РІС‹Р±РµСЂРёС‚Рµ СЃСѓС‰РµСЃС‚РІСѓСЋС‰РёР№ РїСѓРЅРєС‚ РјРµРЅСЋ.\n");
         }
 
     } while (choice != 7);
 }
 
 void Find_Name(struct Materials materials[], int size, char Find_of_Name[]) {
-    printf("\nРезультаты поиска по названию строительного материала \"%s\":\n", Find_of_Name);
+    printf("\nР РµР·СѓР»СЊС‚Р°С‚С‹ РїРѕРёСЃРєР° РїРѕ РЅР°Р·РІР°РЅРёСЋ СЃС‚СЂРѕРёС‚РµР»СЊРЅРѕРіРѕ РјР°С‚РµСЂРёР°Р»Р° \"%s\":\n", Find_of_Name);
     printf("------------------------------------------------\n");
     for (int i = 0; i < size; ++i) {
         if (strstr(materials[i].name, Find_of_Name) != NULL) {
-            printf("|%25s | %10s |\n", "Название", materials[i].name);
-            printf("|%25s | %10d |\n", "Цена", materials[i].price);
-            printf("|%25s | %10.2lf |\n", "Площадь", materials[i].square);
-            printf("|%25s | %10.2lf |\n", "Плотность", materials[i].density);
-            printf("|%25s | %10d |\n", "Прочность", materials[i].endurance);
-            printf("|%25s | %10.2lf |\n", "Теплопроводимость", materials[i].thermal_conductivity);
+            printf("|%25s | %10s |\n", "РќР°Р·РІР°РЅРёРµ", materials[i].name);
+            printf("|%25s | %10d |\n", "Р¦РµРЅР°", materials[i].price);
+            printf("|%25s | %10.2lf |\n", "РџР»РѕС‰Р°РґСЊ", materials[i].square);
+            printf("|%25s | %10.2lf |\n", "РџР»РѕС‚РЅРѕСЃС‚СЊ", materials[i].density);
+            printf("|%25s | %10d |\n", "РџСЂРѕС‡РЅРѕСЃС‚СЊ", materials[i].endurance);
+            printf("|%25s | %10.2lf |\n", "РўРµРїР»РѕРїСЂРѕРІРѕРґРёРјРѕСЃС‚СЊ", materials[i].thermal_conductivity);
             printf("------------------------------------------------\n");
         }
     }
 }
 
 void Find_Price(struct Materials materials[], int size, int Find_of_Price1, int Find_of_Price2) {
-    printf("\nРезультаты поиска по диапазону от \"%d\ до \"%d\:\n", Find_of_Price1, Find_of_Price2);
+    printf("\nР РµР·СѓР»СЊС‚Р°С‚С‹ РїРѕРёСЃРєР° РїРѕ РґРёР°РїР°Р·РѕРЅСѓ РѕС‚ \"%d\ РґРѕ \"%d\:\n", Find_of_Price1, Find_of_Price2);
     printf("------------------------------------------------\n");
     for (int i = 0; i < size; ++i) {
         if (Find_of_Price1 <= materials[i].price && materials[i].price <= Find_of_Price2) {
-            printf("| %25s | %10s |\n", "Название", materials[i].name);
-            printf("| %25s | %10d |\n", "Цена", materials[i].price);
-            printf("| %25s | %10d |\n", "Площадь", materials[i].square);
-            printf("| %25s | %10.2lf |\n", "Плотность", materials[i].density);
-            printf("| %25s | %10d |\n", "Прочность", materials[i].endurance);
-            printf("| %25s | %10.2lf |\n", "Теплопроводимость", materials[i].thermal_conductivity);
+            printf("| %25s | %10s |\n", "РќР°Р·РІР°РЅРёРµ", materials[i].name);
+            printf("| %25s | %10d |\n", "Р¦РµРЅР°", materials[i].price);
+            printf("| %25s | %10d |\n", "РџР»РѕС‰Р°РґСЊ", materials[i].square);
+            printf("| %25s | %10.2lf |\n", "РџР»РѕС‚РЅРѕСЃС‚СЊ", materials[i].density);
+            printf("| %25s | %10d |\n", "РџСЂРѕС‡РЅРѕСЃС‚СЊ", materials[i].endurance);
+            printf("| %25s | %10.2lf |\n", "РўРµРїР»РѕРїСЂРѕРІРѕРґРёРјРѕСЃС‚СЊ", materials[i].thermal_conductivity);
             printf("------------------------------------------------\n");
         }
     }
 }
 void Find_Square(struct Materials materials[], int size, double Find_of_Square) {
-    printf("\nРезультаты поиска по площади \"%s\":\n", Find_of_Square);
+    printf("\nР РµР·СѓР»СЊС‚Р°С‚С‹ РїРѕРёСЃРєР° РїРѕ РїР»РѕС‰Р°РґРё \"%s\":\n", Find_of_Square);
     printf("------------------------------------------------\n");
     for (int i = 0; i < size; ++i) {
         if (materials[i].square == Find_of_Square) {
-            printf("| %25s | %10s |\n", "Название", materials[i].name);
-            printf("| %25s | %10d |\n", "Цена", materials[i].price);
-            printf("| %25s | %10.lf |\n", "Площадь", materials[i].square);
-            printf("| %25s | %10.2lf |\n", "Плотность", materials[i].density);
-            printf("| %25s | %10d |\n", "Прочность", materials[i].endurance);
-            printf("| %25s | %10.2lf |\n", "Теплопроводимость", materials[i].thermal_conductivity);
+            printf("| %25s | %10s |\n", "РќР°Р·РІР°РЅРёРµ", materials[i].name);
+            printf("| %25s | %10d |\n", "Р¦РµРЅР°", materials[i].price);
+            printf("| %25s | %10.lf |\n", "РџР»РѕС‰Р°РґСЊ", materials[i].square);
+            printf("| %25s | %10.2lf |\n", "РџР»РѕС‚РЅРѕСЃС‚СЊ", materials[i].density);
+            printf("| %25s | %10d |\n", "РџСЂРѕС‡РЅРѕСЃС‚СЊ", materials[i].endurance);
+            printf("| %25s | %10.2lf |\n", "РўРµРїР»РѕРїСЂРѕРІРѕРґРёРјРѕСЃС‚СЊ", materials[i].thermal_conductivity);
             printf("------------------------------------------------\n");
         }
     }
 }
 
 void Sort_Thermal_conductivity(struct Materials materials[], int size, double Sort_of_Thermal_conductivity) {
-    printf("\nРезультаты поиска по теплопроводимости до %.2lf:\n", Sort_of_Thermal_conductivity);
+    printf("\nР РµР·СѓР»СЊС‚Р°С‚С‹ РїРѕРёСЃРєР° РїРѕ С‚РµРїР»РѕРїСЂРѕРІРѕРґРёРјРѕСЃС‚Рё РґРѕ %.2lf:\n", Sort_of_Thermal_conductivity);
     printf("------------------------------------------------\n");
     for (int i = 0; i < size; ++i) {
         if (materials[i].thermal_conductivity <= Sort_of_Thermal_conductivity) {
-            printf("| %20s | %25s |\n", "Название", materials[i].name);
-            printf("| %20s | %25d |\n", "Цена", materials[i].price);
-            printf("| %20s | %25.lf |\n", "Площадь", materials[i].square);
-            printf("| %20s | %25.2lf |\n", "Плотность", materials[i].density);
-            printf("| %20s | %25d |\n", "Прочность", materials[i].endurance);
-            printf("| %20s | %25.2lf |\n", "Теплопроводимость", materials[i].thermal_conductivity);
+            printf("| %20s | %25s |\n", "РќР°Р·РІР°РЅРёРµ", materials[i].name);
+            printf("| %20s | %25d |\n", "Р¦РµРЅР°", materials[i].price);
+            printf("| %20s | %25.lf |\n", "РџР»РѕС‰Р°РґСЊ", materials[i].square);
+            printf("| %20s | %25.2lf |\n", "РџР»РѕС‚РЅРѕСЃС‚СЊ", materials[i].density);
+            printf("| %20s | %25d |\n", "РџСЂРѕС‡РЅРѕСЃС‚СЊ", materials[i].endurance);
+            printf("| %20s | %25.2lf |\n", "РўРµРїР»РѕРїСЂРѕРІРѕРґРёРјРѕСЃС‚СЊ", materials[i].thermal_conductivity);
             printf("------------------------------------------------\n");
         }
     }
@@ -191,19 +191,19 @@ void Sort_Thermal_conductivity(struct Materials materials[], int size, double So
 void Write_to_File(struct Materials materials[], int size) {
     FILE* file = fopen("materials.txt", "w");
     if (file == NULL) {
-        printf("Файл не найден.\n");
+        printf("Р¤Р°Р№Р» РЅРµ РЅР°Р№РґРµРЅ.\n");
         return;
     }
     printf("------------------------------------------------\n");
     for (int i = 0; i < size; ++i) {
-        fprintf(file, "|%20s | %15s |\n", "Название", materials[i].name);
-        fprintf(file, "|%20s | %15d |\n", "Цена", materials[i].price);
-        fprintf(file, "|%20s | %15.2lf |\n", "Площадь", materials[i].square);
-        fprintf(file, "|%20s | %15.2lf |\n", "Плотность", materials[i].density);
-        fprintf(file, "|%20s | %15d |\n", "Прочность", materials[i].endurance);
-        fprintf(file, "|%20s | %15.2lf |\n", "Теплопроводимость", materials[i].thermal_conductivity);
+        fprintf(file, "|%20s | %15s |\n", "РќР°Р·РІР°РЅРёРµ", materials[i].name);
+        fprintf(file, "|%20s | %15d |\n", "Р¦РµРЅР°", materials[i].price);
+        fprintf(file, "|%20s | %15.2lf |\n", "РџР»РѕС‰Р°РґСЊ", materials[i].square);
+        fprintf(file, "|%20s | %15.2lf |\n", "РџР»РѕС‚РЅРѕСЃС‚СЊ", materials[i].density);
+        fprintf(file, "|%20s | %15d |\n", "РџСЂРѕС‡РЅРѕСЃС‚СЊ", materials[i].endurance);
+        fprintf(file, "|%20s | %15.2lf |\n", "РўРµРїР»РѕРїСЂРѕРІРѕРґРёРјРѕСЃС‚СЊ", materials[i].thermal_conductivity);
         fprintf(file, "------------------------------------------------\n");
     }
     fclose(file);
-    printf("Данные были записаны в файл.\n");
+    printf("Р”Р°РЅРЅС‹Рµ Р±С‹Р»Рё Р·Р°РїРёСЃР°РЅС‹ РІ С„Р°Р№Р».\n");
 }
